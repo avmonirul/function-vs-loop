4654e69646e75cabd24cf7e00606802e6c2810fb
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-31 02:10:51 +0600

    first commit

--- object-lopping.js
+++ object-lopping.js
@@ -1,5 +1,5 @@
 //array vs object
-var soppingItems = ['books', 'sunglass', 'shoes', 'pen'];
+var shoppingItems = ['books', 'sunglass', 'shoes', 'pen'];
 var friendsAge = [12, 45, 78, 12, 32, 14];
 var friendsAge ={
         rahim: 12,
@@ -10,18 +10,26 @@ var friendsAge ={
         kazi: 14
 }
 
-var soppingCart = {
+var shoppingCart = {
     books: 3,
     sunglass: 1,
     keyboard: 5,
     mouse: 1,
     pen: 25,
     shoes:2,
-    bottle:1
 }
 
-const key = Object.keys(soppingCart);
-console.log(key);
+const keys = Object.keys(shoppingCart);
+console.log(keys);
 
-const values = Object.values(soppingCart);
+const values = Object.values(shoppingCart);
 console.log(values);
+
+//var key = [ 'books', 'sunglass', 'keyboard', 'mouse', 'pen', 'shoes' ]
+for(var i = 0; i < keys.length; i++){
+    var propertyName = keys[i];
+    var propertyValue = shoppingCart[propertyName];
+    console.log(propertyName,propertyValue);
+}
+
+//for in loop

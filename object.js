a2eb62ffc5ee59300f4a15d136d98b7d13119077
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-30 23:51:23 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,13 @@
+var student ={ 
+    id:115, 
+    name:'Solamin khan', 
+    class: 9, 
+    marks:98
+}
+
+var mobile ={
+    brand: 'samsung',
+    price: 19000,
+    Storage:'128gb',
+    camera:'7px'
+}

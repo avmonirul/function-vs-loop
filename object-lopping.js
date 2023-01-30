f10c3ab3c3c36ce8b6dc03f57a3f8e734d62299b
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-31 02:18:03 +0600

    first commit

--- object-lopping.js
+++ object-lopping.js
@@ -29,7 +29,11 @@ console.log(values);
 for(var i = 0; i < keys.length; i++){
     var propertyName = keys[i];
     var propertyValue = shoppingCart[propertyName];
-    console.log(propertyName,propertyValue);
+    // console.log(propertyName,propertyValue);
 }
 
 //for in loop
+for(var propertyName in shoppingCart){
+    const value = shoppingCart[propertyName];
+    console.log(propertyName);
+}

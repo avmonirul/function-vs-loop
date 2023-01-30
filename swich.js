03e40e24e787e9fc3bd07292e921d300d547ad84
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-31 03:03:19 +0600

    first commit

---
+++ swich.js
@@ -0,0 +1,41 @@
+const color = 'yellow';
+if(color === 'green'){
+    console.log('you are a green friends');
+}
+else if (color === 'blue'){
+    console.log('you are a blue friends');
+}
+else if (color === 'red'){
+    console.log('you are my red friends');
+}
+else if (color === 'white'){
+    console.log('you are my white friends');
+}
+else if (color === 'yellow'){
+    console.log('you are my red friends');
+}
+else{
+    console.log('you are my black friends');
+}
+
+//switch
+switch(color){
+    case 'green':
+        console.log('you are my green friends');
+        break;
+    case 'blue':
+        console.loge('you are my blue friends');
+        break;
+    case 'white':
+        console.log('you are my white friends');
+        break;
+    case 'red':
+        console.log('you are my red friends');
+        break;
+    case 'yellow':
+        console.log('you are my yellow friends');
+        break;
+    default:
+        console.log('you are my kaal friends');
+        break;
+}

b33ac20a9a56ee3f6260df749d0cf66ae43ec6e1
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-31 01:42:33 +0600

    first commit

---
+++ object-lopping.js
@@ -0,0 +1,20 @@
+//array vs object
+var soppingItems = ['books', 'sunglass', 'shoes', 'pen'];
+var friendsAge = [12, 45, 78, 12, 32, 14];
+var friendsAge ={
+        rahim: 12,
+        samad: 45,
+        karim: 78,
+        jamal: 12,
+        robi: 32,
+        kazi: 14
+}
+
+var soppingCart = {
+    books: 3,
+    sunglass: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25,
+    shoes:2
+}
